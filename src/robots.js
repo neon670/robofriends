11175ce333c1109d5c0d7f321577020f56afcd27
noe nevarez <closner_nevarez@hotmail.com>
2019-04-13 20:15:34 +0700

    added robo cardlist

---
+++ src/robots.js
@@ -0,0 +1,28 @@
+export const robots = [
+{
+	id:1,
+	name:'John Doe',
+	username: 'John',
+	email: 'me@me.com'
+},
+{
+	id:2,
+	name:'John Doer',
+	username: 'Joe',
+	email: 'me@met.com'
+},
+{
+	id:3,
+	name:'Jo Doer',
+	username: 'Me',
+	email: 'met@met.com'
+},
+{
+	id:4,
+	name:'Jo Doer',
+	username: 'Meeet',
+	email: 'met@moemoney.co'
+},
+
+
+]
