63c268416a273069ddaa537d4c52b6ca368ceb8c
noe nevarez <closner_nevarez@hotmail.com>
2019-04-28 22:43:24 +0700

    added new actions and reducers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,44 +1,61 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import	'./App.css';
+import { setSearchField, requestRobots } from '../action';
+
+const mapStateToProps = state => {
+	return{
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
+	}
+}
+const mapDispatchToProps =(dispatch) =>{
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	}
+}
 
 class App extends Component{
 
-	constructor(){
-		super()
-		this.state ={
-			robots: [],
-			searchfield: ''
-		}
-	}
+	// constructor(){
+	// 	super()
+	// 	this.state ={
+	// 		robots: [],
+	// 	}
+	// }
 
 	componentDidMount(){
-		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response=> response.json())
-		.then(users => this.setState({ robots: users}));
-		
+		// fetch('https://jsonplaceholder.typicode.com/users')
+		// .then(response=> response.json())
+		// .then(users => this.setState({ robots: users}));
+		this.props.onRequestRobots();
 	}
 
-	onSearchChange= (event) => {
-		this.setState({searchfield: event.target.value})
+	// onSearchChange= (event) => {
+	// 	this.setState({searchfield: event.target.value})
 		
-	}
+	// }
 
 	render(){
-		const { robots, searchfield } = this.state;
+			// const { robots } = this.state;
+			const { searchField, onSearchChange, robots, isPending } = this.props;
 			const filteredRobots = robots.filter(robot=>{
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		})
 
-		return  !robots.length ?
+		return  isPending ?
 			<h1>Loading</h1> :
 		(
 		<div className='tc'>
 			<h1 className='f1'>RoboFriends</h1>
-			<SearchBox searchChange={this.onSearchChange}/>
+			<SearchBox searchChange={ onSearchChange }/>
 			<Scroll>
 				<ErrorBoundry>
 					<CardList robots={ filteredRobots } />
@@ -48,4 +65,4 @@ class App extends Component{
 		);
 	}
 }
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
