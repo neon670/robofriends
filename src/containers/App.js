2366cc66447868c849c8042d2667706740c629ab
noe nevarez <closner_nevarez@hotmail.com>
2019-05-06 22:02:14 +0700

    final update

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
+import Header from '../components/Header';
 import	'./App.css';
 import { setSearchField, requestRobots } from '../action';
 
@@ -54,7 +55,7 @@ class App extends Component{
 			<h1>Loading</h1> :
 		(
 		<div className='tc'>
-			<h1 className='f1'>RoboFriends</h1>
+			<Header/>
 			<SearchBox searchChange={ onSearchChange }/>
 			<Scroll>
 				<ErrorBoundry>
